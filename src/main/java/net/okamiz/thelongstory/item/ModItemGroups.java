e51a907623812e4a06e440a37561e5bc1585d9a5
Okamiz <jojo.berkes@gmail.com>
2023-10-18 22:05:43 +0200

    Added Item Group

---
+++ src/main/java/net/okamiz/thelongstory/item/ModItemGroups.java
@@ -0,0 +1,27 @@
+package net.okamiz.thelongstory.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import net.okamiz.thelongstory.TheLongStory;
+
+public class ModItemGroups {
+    public static final ItemGroup MOD_GROUP = Registry.register(Registries.ITEM_GROUP,
+            new Identifier(TheLongStory.MOD_ID, "modgroup"),
+            FabricItemGroup.builder().displayName(Text.translatable("itemgroup.modgroup"))
+                    .icon(() -> new ItemStack(ModItems.BONE_FRAGMENTS)).entries((displayContext, entries) -> {
+
+                        entries.add(ModItems.BONE_FRAGMENTS);
+                        entries.add(ModItems.CRUSHED_BONES);
+
+
+                    }).build());
+    public static void registerItemGroups(){
+        TheLongStory.LOGGER.info("Registering Item Groups for " + TheLongStory.MOD_ID);
+    }
+
+}
