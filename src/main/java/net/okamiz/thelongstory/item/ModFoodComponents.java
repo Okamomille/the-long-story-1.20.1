adde36542095709716e8234e4388539f4572f902
Okamiz <jojo.berkes@gmail.com>
2023-10-19 21:05:33 +0200

    Added APPLES

--- src/main/java/net/okamiz/thelongstory/item/ModFoodComponents.java
+++ src/main/java/net/okamiz/thelongstory/item/ModFoodComponents.java
@@ -5,15 +5,15 @@ import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.FoodComponent;
 
 public class ModFoodComponents {
-    public static final FoodComponent EMERALD_APPLE = new FoodComponent.Builder().alwaysEdible().hunger(6).saturationModifier(1.2f)
+    public static final FoodComponent EMERALD_APPLE = new FoodComponent.Builder().alwaysEdible().hunger(8).saturationModifier(1.2f)
             .statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 3600),100)
             .statusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 2400,1),100).build();
 
-    public static final FoodComponent IRON_APPLE = new FoodComponent.Builder().alwaysEdible().hunger(6).saturationModifier(1.2f)
+    public static final FoodComponent IRON_APPLE = new FoodComponent.Builder().alwaysEdible().hunger(3).saturationModifier(1.2f)
             .statusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 1200),100)
             .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 50,1),100).build();
 
-    public static final FoodComponent DIAMOND_APPLE = new FoodComponent.Builder().alwaysEdible().hunger(6).saturationModifier(1.2f)
+    public static final FoodComponent DIAMOND_APPLE = new FoodComponent.Builder().alwaysEdible().hunger(10).saturationModifier(1.2f)
             .statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 3600),100)
             .statusEffect(new StatusEffectInstance(StatusEffects.WATER_BREATHING, 2400),100)
             .statusEffect(new StatusEffectInstance(StatusEffects.DOLPHINS_GRACE, 2400, 1),100).build();
@@ -22,11 +22,11 @@ public class ModFoodComponents {
             .statusEffect(new StatusEffectInstance(StatusEffects.FIRE_RESISTANCE, 3600),100)
             .statusEffect(new StatusEffectInstance(StatusEffects.HASTE, 2400,1),100).build();
 
-    public static final FoodComponent LAPIS_APPLE = new FoodComponent.Builder().alwaysEdible().hunger(6).saturationModifier(1.2f)
+    public static final FoodComponent LAPIS_APPLE = new FoodComponent.Builder().alwaysEdible().hunger(4).saturationModifier(1.2f)
             .statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 3600),100)
             .statusEffect(new StatusEffectInstance(StatusEffects.JUMP_BOOST, 3600),100).build();
 
-    public static final FoodComponent AMETHYST_APPLE = new FoodComponent.Builder().alwaysEdible().hunger(6).saturationModifier(1.2f)
+    public static final FoodComponent AMETHYST_APPLE = new FoodComponent.Builder().alwaysEdible().hunger(5).saturationModifier(1.2f)
             .statusEffect(new StatusEffectInstance(StatusEffects.INVISIBILITY, 3600, 1, false, false, true),100)
             .statusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 2400,1, false, false, true),100).build();
 }
