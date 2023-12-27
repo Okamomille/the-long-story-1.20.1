f57353c7fa896eda48ce7307aa558e8a68ca8bbf
Okamomille <jojo.berkes@gmail.com>
2023-12-27 18:22:43 +0100

    Added Ore Generation

--- src/main/java/net/okamiz/thelongstory/TheLongStoryDataGenerator.java
+++ src/main/java/net/okamiz/thelongstory/TheLongStoryDataGenerator.java
@@ -2,7 +2,11 @@ package net.okamiz.thelongstory;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.minecraft.registry.RegistryBuilder;
+import net.minecraft.registry.RegistryKeys;
 import net.okamiz.thelongstory.datagen.*;
+import net.okamiz.thelongstory.world.gen.ModConfiguredFeatures;
+import net.okamiz.thelongstory.world.gen.ModPlacedFeatures;
 
 public class TheLongStoryDataGenerator implements DataGeneratorEntrypoint {
 	@Override
@@ -14,5 +18,12 @@ public class TheLongStoryDataGenerator implements DataGeneratorEntrypoint {
 		pack.addProvider(ModModelProvider::new);
 		pack.addProvider(ModRecipeProvider::new);
 		pack.addProvider(ModLootTableProvider::new);
+		pack.addProvider(ModWorldGenerator::new);
+	}
+
+	@Override
+	public void buildRegistry(RegistryBuilder registryBuilder) {
+		registryBuilder.addRegistry(RegistryKeys.CONFIGURED_FEATURE, ModConfiguredFeatures::boostrap);
+		registryBuilder.addRegistry(RegistryKeys.PLACED_FEATURE, ModPlacedFeatures::boostrap);
 	}
 }
---
+++ src/main/java/net/okamiz/thelongstory/world/gen/ModConfiguredFeatures.java
@@ -0,0 +1,46 @@
+package net.okamiz.thelongstory.world.gen;
+
+import net.minecraft.registry.Registerable;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.structure.rule.RuleTest;
+import net.minecraft.structure.rule.TagMatchRuleTest;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.FeatureConfig;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
+import net.okamiz.thelongstory.TheLongStory;
+import net.okamiz.thelongstory.block.ModBlocks;
+
+import java.util.List;
+
+public class ModConfiguredFeatures {
+
+    public static final RegistryKey<ConfiguredFeature<?, ?>> THESTONE_ORE_KEY = registryKey("thestone_ore");
+
+
+    public static void boostrap(Registerable<ConfiguredFeature<?,?>> context){
+        RuleTest stoneReplacables = new TagMatchRuleTest(BlockTags.STONE_ORE_REPLACEABLES);
+        RuleTest deepslateReplacables = new TagMatchRuleTest(BlockTags.DEEPSLATE_ORE_REPLACEABLES);
+
+
+        List<OreFeatureConfig.Target> overworldThestoneOres =
+                List.of(OreFeatureConfig.createTarget(stoneReplacables, ModBlocks.THESTONE_ORE.getDefaultState()),
+                        OreFeatureConfig.createTarget(deepslateReplacables, ModBlocks.DEEPSLATE_THESTONE_ORE.getDefaultState()));
+
+
+        register(context, THESTONE_ORE_KEY, Feature.ORE, new OreFeatureConfig(overworldThestoneOres, 7));
+    }
+
+
+    public static RegistryKey<ConfiguredFeature<?,?>> registryKey(String name){
+        return RegistryKey.of(RegistryKeys.CONFIGURED_FEATURE, new Identifier(TheLongStory.MOD_ID, name));
+    }
+
+    private static <FC extends FeatureConfig, F extends Feature<FC>> void register(Registerable<ConfiguredFeature<?, ?>> context,
+                                                                                   RegistryKey<ConfiguredFeature<?, ?>> key, F feature, FC configuration){
+        context.register(key, new ConfiguredFeature<>(feature, configuration));
+    }
+}
