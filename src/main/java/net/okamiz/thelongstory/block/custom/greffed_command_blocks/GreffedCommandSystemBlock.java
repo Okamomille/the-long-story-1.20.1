3ba6b1beadf04d5901bcc030a6365904962fdb13
Okamomille <jojo.berkes@gmail.com>
2023-12-29 14:31:22 +0100

    Changed the command system blocks classes.

--- src/main/java/net/okamiz/thelongstory/block/custom/greffed_command_blocks/GreffedCommandSystemBlock.java
+++ src/main/java/net/okamiz/thelongstory/block/custom/greffed_command_blocks/GreffedCommandSystemBlock.java
@@ -1,19 +1,14 @@
-package net.okamiz.thelongstory.block.custom;
+package net.okamiz.thelongstory.block.custom.greffed_command_blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.item.TooltipContext;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.potion.Potions;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
-import net.minecraft.stat.Stat;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
@@ -26,52 +21,52 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
-public class PinkGreffedCommandSystemBlock extends Block{
-    public PinkGreffedCommandSystemBlock(Settings settings) {
+public class GreffedCommandSystemBlock extends Block{
+
+    private float timer = 0;
+    private final float timerdelay = 3600;
+    public final StatusEffectInstance effectInstance = new StatusEffectInstance(StatusEffects.REGENERATION, 3600, 1);
+
+    public GreffedCommandSystemBlock(Settings settings) {
         super(settings);
         }
-        private float timer = 0;
-        private final float timerdelay = 3600;
+
 
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit){
 
-
-
-
-
-        if(timer <= 0){
+        if(this.timer <= 0){
             if(!world.isClient()){
-                player.addStatusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 3600, 1));
-                timer = timerdelay;
+                applyEffect(player, effectInstance);
+                this.timer = timerdelay;
             }
             world.playSound(player, pos, SoundEvents.ENTITY_ALLAY_AMBIENT_WITH_ITEM, SoundCategory.BLOCKS, 1f, 1.2f);
             world.playSound(player, pos, SoundEvents.ITEM_LODESTONE_COMPASS_LOCK, SoundCategory.BLOCKS, 1f, 1f);
 
-        }else if (timer > 0){
+        }else if (this.timer > 0){
             //WRONG
             if(!world.isClient()){
-            player.sendMessage(Text.literal("§o§7The Command System is cooling, you have to wait. " + timer));
+            player.sendMessage(Text.literal("§o§7The Command System is cooling, you have to wait. " + this.timer));
             }
             world.playSound(player, pos, SoundEvents.BLOCK_CHEST_LOCKED, SoundCategory.BLOCKS, 1f, 2f);
         }
-
-
         return ActionResult.SUCCESS;
     }
 
+
+    public void applyEffect(PlayerEntity player, StatusEffectInstance instance){
+
+        player.addStatusEffect(instance);
+
+    }
+
+
     @Override
     public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
-        if(timer > 0){
-            timer --;
+        if(this.timer > 0){
+            this.timer --;
         }
         super.randomDisplayTick(state, world, pos, random);
     }
 
-
-    @Override
-    public void appendTooltip(ItemStack stack, @Nullable BlockView world, List<Text> tooltip, TooltipContext options) {
-        tooltip.add(Text.translatable("tooltip.thelongstory.pink_greffed_command_system"));
-        super.appendTooltip(stack, world, tooltip, options);
-    }
 }
