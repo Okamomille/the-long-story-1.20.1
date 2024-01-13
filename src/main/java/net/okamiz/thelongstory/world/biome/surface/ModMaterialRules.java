78eb6455a4f4120e8503666eff3e1f6555a7d1f0
Okamomille <jojo.berkes@gmail.com>
2024-01-13 18:15:47 +0100

    Added Deep Ice to the Snow Forest Biome

--- src/main/java/net/okamiz/thelongstory/world/biome/surface/ModMaterialRules.java
+++ src/main/java/net/okamiz/thelongstory/world/biome/surface/ModMaterialRules.java
@@ -13,31 +13,27 @@ public class ModMaterialRules {
     private static final MaterialRules.MaterialRule PACKED_ICE = makeStateRule(Blocks.PACKED_ICE);
     private static final MaterialRules.MaterialRule SNOW_BLOCK = makeStateRule(Blocks.SNOW_BLOCK);
 
-    private static final MaterialRules.MaterialRule DIRT = makeStateRule(Blocks.DIRT);
     private static final MaterialRules.MaterialRule BEDROCK = makeStateRule(Blocks.BEDROCK);
     private static final MaterialRules.MaterialRule MAGMA_BLOCK = makeStateRule(Blocks.MAGMA_BLOCK);
     private static final MaterialRules.MaterialRule LAVA = makeStateRule(Blocks.LAVA);
-    private static final MaterialRules.MaterialRule GRASS_BLOCK = makeStateRule(Blocks.GRASS_BLOCK);
-    private static final MaterialRules.MaterialRule STONE = makeStateRule(Blocks.STONE);
+    private static final MaterialRules.MaterialRule DEEP_ICE = makeStateRule(ModBlocks.DEEP_ICE);
 
     public static MaterialRules.MaterialRule makeRules() {
-        MaterialRules.MaterialCondition isAtOrAboveWaterLevel = MaterialRules.water(-1, 0);
-
-        MaterialRules.MaterialRule snowSurface = MaterialRules.sequence(MaterialRules.condition(isAtOrAboveWaterLevel, SNOW_BLOCK), PACKED_ICE);
-        MaterialRules.MaterialRule grassSurface = MaterialRules.sequence(MaterialRules.condition(isAtOrAboveWaterLevel, GRASS_BLOCK), DIRT);
 
         return MaterialRules.sequence(
                 MaterialRules.condition(MaterialRules.verticalGradient
                         ("snow_forest_bedrock", YOffset.aboveBottom(0),YOffset.aboveBottom(3)), BEDROCK),
                  MaterialRules.condition(MaterialRules.verticalGradient
-                        ("snow_forest_bedrock", YOffset.aboveBottom(3),YOffset.aboveBottom(6)), MAGMA_BLOCK),
+                        ("snow_forest_magma", YOffset.aboveBottom(3),YOffset.aboveBottom(6)), MAGMA_BLOCK),
                 MaterialRules.condition(MaterialRules.verticalGradient
-                        ("snow_forest_bedrock", YOffset.aboveBottom(5),YOffset.aboveBottom(10)), LAVA),
+                        ("snow_forest_lava", YOffset.aboveBottom(5),YOffset.aboveBottom(10)), LAVA),
 
 
-
+                MaterialRules.condition(MaterialRules.biome(ModBiomes.SNOW_FOREST),(MaterialRules.condition(MaterialRules.verticalGradient
+                        ("snow_forest_bedrock", YOffset.aboveBottom(10),YOffset.aboveBottom(64)), DEEP_ICE))),
                 MaterialRules.condition(MaterialRules.biome(ModBiomes.SNOW_FOREST), MaterialRules.condition(MaterialRules.STONE_DEPTH_FLOOR, SNOW_BLOCK)),
                 MaterialRules.condition(MaterialRules.biome(ModBiomes.SNOW_FOREST), PACKED_ICE)
+
         );
 
 
